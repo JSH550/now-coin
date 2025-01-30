89843c15ecfc22b0faeb303f0407ba95afd5102a
JSH <skyjs135@gmail.com>
2025-01-30 17:01:06 +0900

    feat: 리프레시 토큰 Redis 저장 방식으로 변경 - CustomSuccessHandler에서 리프레시 토큰을 Redis에만 저장하도록 수정

--- src/main/java/com/coin/now_coin/common/auth/CustomSuccessHandler.java
+++ src/main/java/com/coin/now_coin/common/auth/CustomSuccessHandler.java
@@ -2,6 +2,8 @@ package com.coin.now_coin.common.auth;
 
 
 import com.coin.now_coin.common.auth.dto.CustomOAuth2User;
+import com.coin.now_coin.common.jwt.JwtUtil;
+import com.coin.now_coin.common.jwt.RefreshTokenService;
 import com.coin.now_coin.member.MemberRole;
 import com.coin.now_coin.member.dto.CreateMemberJwtDto;
 import jakarta.servlet.ServletException;
@@ -25,7 +27,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
     private final JwtUtil jwtUtil;
 
-    private final  RefreshTokenService refreshTokenService;
+    private final RefreshTokenService refreshTokenService;
 
     public CustomSuccessHandler(JwtUtil jwtUtil, RefreshTokenService refreshTokenService) {
         this.jwtUtil = jwtUtil;
@@ -56,20 +58,15 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         Cookie jwtCookie = new Cookie("ACCESS_TOKEN", accessToken);
         jwtCookie.setHttpOnly(true); // JavaScript에서 접근 불가
         jwtCookie.setPath("/"); // 전체 도메인에서 쿠키 유효
-        jwtCookie.setMaxAge((int) jwtUtil.getExpirationTime()); // 쿠키 만료 시간 (초 단위)
+        jwtCookie.setMaxAge(3600); // 쿠키 만료 시간 1시간
 
 
 
         //리프레시 토큰 생성
-        String refreshToken = jwtUtil.generateRefreshToken(providerId);
-        Cookie refreshCookie = new Cookie("REFRESH_TOKEN",refreshToken);
-        refreshCookie.setHttpOnly(true);
-        refreshCookie.setPath("/");
-        jwtCookie.setMaxAge((int) jwtUtil.getExpirationTime()); // 쿠키 만료 시간 (초 단위)
+        String refreshToken = jwtUtil.generateRefreshToken(providerId,name);
         refreshTokenService.saveRefreshTokenToRedis(providerId,refreshToken);//Redis에 토큰 저장
 
         response.addCookie(jwtCookie);//응답에 엑세스 토큰 추가
-        response.addCookie(refreshCookie);//응답에 리프레시 토큰 추가
         response.sendRedirect(domain);//응답 반환
 
 
